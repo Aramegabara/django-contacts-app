13e7453a2757b41f9e55fee9cd54c802f1e5054a
Artur Heraskov <geraskov94@gmail.com>
2026-01-14 02:42:24 +0100

    Add PythonAnywhere domain to ALLOWED_HOSTS

--- contacts_project/settings.py
+++ contacts_project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ageraskov.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
